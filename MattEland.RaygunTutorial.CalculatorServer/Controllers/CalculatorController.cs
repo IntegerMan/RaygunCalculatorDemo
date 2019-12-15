af1b90a26773b7c1c00a911c43159c9fa465b8c4
Matt Eland <Matt.Eland@GMail.com>
2019-12-15 16:00:44 -0500

    Simplified the application profile for demo purposes.

---
+++ MattEland.RaygunTutorial.CalculatorServer/Controllers/CalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MattEland.RaygunTutorial.CalculatorServer.Controllers
+{
+    [ApiController]
+    [Route("calculator")]
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        [Route("add/{x}/{y}")]
+        public decimal Add(decimal x, decimal y) => x + y;
+
+        [HttpGet]
+        [Route("subtract/{x}/{y}")]
+        public decimal Subtract(decimal x, decimal y) => x - y;
+
+        [HttpGet]
+        [Route("multiply/{x}/{y}")]
+        public decimal Multiply(decimal x, decimal y) => x * y;
+
+        [HttpGet]
+        [Route("divide/{x}/{y}")]
+        public decimal Divide(decimal x, decimal y) => x / y;
+
+        [HttpGet]
+        [Route("power/{x}/{y}")]
+        public decimal Exponent(decimal x, decimal y) => (decimal) Math.Pow((double)x, (double)y);
+    }
+}
