27a17d9b3d5c1398b077e34dd2787035aca0283e
Matt Eland <Matt.Eland@GMail.com>
2019-12-15 18:22:46 -0500

    Added a bit of fragility to the controller as a straw man argument

--- MattEland.RaygunTutorial.CalculatorServer/Controllers/CalculatorController.cs
+++ MattEland.RaygunTutorial.CalculatorServer/Controllers/CalculatorController.cs
@@ -9,22 +9,52 @@ namespace MattEland.RaygunTutorial.CalculatorServer.Controllers
     {
         [HttpGet]
         [Route("add/{x}/{y}")]
-        public decimal Add(decimal x, decimal y) => x + y;
+        public decimal Add(string x, string y)
+        {
+            var xVal = decimal.Parse(x);
+            var yVal = decimal.Parse(y);
+
+            return xVal + yVal;
+        }
 
         [HttpGet]
         [Route("subtract/{x}/{y}")]
-        public decimal Subtract(decimal x, decimal y) => x - y;
+        public decimal Subtract(string x, string y)
+        {
+            var xVal = decimal.Parse(x);
+            var yVal = decimal.Parse(y);
+
+            return xVal - yVal;
+        }
 
         [HttpGet]
         [Route("multiply/{x}/{y}")]
-        public decimal Multiply(decimal x, decimal y) => x * y;
+        public decimal Multiply(string x, string y)
+        {
+            var xVal = decimal.Parse(x);
+            var yVal = decimal.Parse(y);
+
+            return xVal * yVal;
+        }
 
         [HttpGet]
         [Route("divide/{x}/{y}")]
-        public decimal Divide(decimal x, decimal y) => x / y;
+        public decimal Divide(string x, string y)
+        {
+            var xVal = decimal.Parse(x);
+            var yVal = decimal.Parse(y);
+
+            return xVal / yVal;
+        }
 
         [HttpGet]
         [Route("power/{x}/{y}")]
-        public decimal Exponent(decimal x, decimal y) => (decimal) Math.Pow((double)x, (double)y);
+        public double Exponent(string x, string y)
+        {
+            var num = double.Parse(x);
+            var power = double.Parse(y);
+
+            return Math.Pow(num, power);
+        }
     }
 }
